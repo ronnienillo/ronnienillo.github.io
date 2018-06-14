b2b87c66535b73d28d4175ee2bf57b08a808ef17
ronnienillo <nilloronniecolayo@gmail.com>
2018-06-14 19:12:00 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,36 @@
+self.addEventListener('install', function (event) {
+  console.log('SW Installed');
+  event.waitUntil(
+    caches.open('static')
+      .then(function (cache) {
+        // cache.add('/');
+        // cache.add('/index.html');
+        // cache.add('/src/js/app.js');
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/js/app.js',
+          '/css/style.css',
+          '/favicon.png',
+          'https://fonts.googleapis.com/css?family=Dosis|Gloria+Hallelujah'
+        ]);
+      })
+  );
+});
+
+self.addEventListener('activate', function () {
+  console.log('SW Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) {
+          return res;
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
+});
